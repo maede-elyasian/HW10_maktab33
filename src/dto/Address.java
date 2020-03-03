147d51990a7767009ef2fb6d8e4acd8749cfbe6d
Maede <maede25en@gmail.com>
2020-03-03 16:12:44 +0330

    Add Address class to dto

---
+++ src/dto/Address.java
@@ -0,0 +1,58 @@
+package dto;
+
+public class Address {
+    private int id;
+    private String state;
+    private String city;
+    private String street;
+    private String postalCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        String address = "{ state:" + state + "," +
+                "city:" + city + "," +
+                "street:" + street + "," +
+                "postal code:" + postalCode + "}";
+        return address;
+    }
+}
