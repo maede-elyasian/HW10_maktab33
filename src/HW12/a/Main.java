eb5b7963ba2d06fa154dcc8a1b19b2b6ff43b2c6
Maede <maede25en@gmail.com>
2020-04-24 16:08:20 +0430

    finish sorting ages

--- src/HW12/a/Main.java
+++ src/HW12/a/Main.java
@@ -4,6 +4,9 @@ package HW12.a;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("sorting users age");
+        System.out.println(SortAge.sortUsersAge());
+
 
 
     }
--- src/HW12/a/SortAge.java
+++ src/HW12/a/SortAge.java
@@ -2,24 +2,30 @@ package HW12.a;
 
 import dao.UserDao;
 import dto.User;
-
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
-import java.util.List;
+
 
 public class SortAge {
-    static ArrayList<User> userList = new ArrayList<>();
+    static ArrayList<User> userList;
 
     public static ArrayList<User> sortUsersAge() {
         UserDao userDao = new UserDao();
-
         try {
             HashSet<User> users = userDao.allUsers();
+            userList = new ArrayList<>(users);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        Comparator<User> userComparator =(u1,u2)->u1.getAge()>u2.getAge()? 1 : u1.getAge() < u2.getAge() ? -1 : 0;
+        Collections.sort(userList,userComparator);
         return userList;
     }
+
+
 
 }
